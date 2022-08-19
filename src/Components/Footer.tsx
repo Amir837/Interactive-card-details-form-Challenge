f0bd324f5e4e4c2a49f5e1ba306522e75ef3e143
Amir <amir.maksyutov@outlook.com>
2022-08-19 16:03:38 -0700

    Footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,7 +1,13 @@
+import "../styles/Footer.css";
 export default function Footer() {
-    return(
+    return (
         <div>
-            <h1>Footer</h1>
+            <footer>
+                <p>
+                    Challenge by <a className="frontendMentor" href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+                    Coded by <a className="self" href="https://github.com/Amir837">Amir</a>.
+                </p>
+            </footer>
         </div>
     );
 }
